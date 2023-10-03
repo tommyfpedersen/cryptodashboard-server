8621d1f4f25a6748ad18aae7a12ad7340c3ae6d4
tommyfpedersen <tommy@faldt.net>
2023-10-03 21:04:31 +0200

    hbs added and assets

--- index.js
+++ index.js
@@ -7,22 +7,45 @@ const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 app.use(express.json());
 app.use(cors({
-    origin: '*' // <-- which domains can access this API
+  origin: '*' // <-- which domains can access this API
 }));
 
 /* routes */
-const addressindexRouter = require('./routes/addressindex');
-app.use('/addressindex', addressindexRouter);
+// const addressindexRouter = require('./routes/addressindex');
+// app.use('/addressindex', addressindexRouter);
 
-const miningRouter = require('./routes/mining');
-app.use('/mining', miningRouter);
+// const miningRouter = require('./routes/mining');
+// app.use('/mining', miningRouter);
 
-const networkRouter = require('./routes/network');
-app.use('/network', networkRouter);
+// const networkRouter = require('./routes/network');
+// app.use('/network', networkRouter);
 
-app.get('/', (req, res) => {
-  res.send('verus rest api')
+app.get('/', async (req, res) => {
+
+  const getmininginfoResponse = await fetch("http://localhost:9009/mining/getmininginfo")
+  const getmininginfoResult = await getmininginfoResponse.json();
+  const getmininginfo = getmininginfoResult.result;
+
+  console.log("getmininginfo", getmininginfo)
+
+  res.render('main',{
+    blocks: getmininginfo.blocks,
+    averageblockfees: getmininginfo.averageblockfees
+  })
 })
+
+/* hbs */
+const hbs = require('hbs')
+app.set('views', './views')
+app.set('view engine', 'hbs')
+
+app.use(express.static(__dirname + "/public", {
+  index: false, 
+  immutable: true, 
+  cacheControl: true,
+  maxAge: "30d"
+}));
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
