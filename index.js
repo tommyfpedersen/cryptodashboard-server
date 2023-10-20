92e433d4e89a939ecfc4beb8fb1c4610e12ac0d3
tommyfpedersen <tommy@faldt.net>
2023-10-20 21:26:33 +0200

    stability

--- index.js
+++ index.js
@@ -114,30 +114,38 @@ app.get('/', async (req, res) => {
       const daiPriceResult = await daiPriceResponse.json();
       const daiPrice = daiPriceResult;
 
-      priceArray.push({
-        currencyId: "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM",
-        price: daiPrice.market_data.current_price.usd
-      })
+      if(daiPrice){
+        priceArray.push({
+          currencyId: "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM",
+          price: daiPrice.market_data.current_price.usd
+        })
+      }
+    
 
       //MKR
       let mkrPriceResponse = await fetch("https://api.coingecko.com/api/v3/coins/maker");
       const mkrPriceResult = await mkrPriceResponse.json();
       const mkrPrice = mkrPriceResult;
 
-      priceArray.push({
-        currencyId: "iCkKJuJScy4Z6NSDK7Mt42ZAB2NEnAE1o4",
-        price: mkrPrice.market_data.current_price.usd
-      })
-
+      if(mkrPrice){
+        priceArray.push({
+          currencyId: "iCkKJuJScy4Z6NSDK7Mt42ZAB2NEnAE1o4",
+          price: mkrPrice.market_data.current_price.usd
+        })
+      }
+     
       //ETH
       let ethPriceResponse = await fetch("https://api.coingecko.com/api/v3/coins/ethereum");
       const ethPriceResult = await ethPriceResponse.json();
       const ethPrice = ethPriceResult;
 
-      priceArray.push({
-        currencyId: "i9nwxtKuVYX4MSbeULLiK2ttVi6rUEhh4X",
-        price: ethPrice.market_data.current_price.usd
-      })
+      if(ethPrice){
+        priceArray.push({
+          currencyId: "i9nwxtKuVYX4MSbeULLiK2ttVi6rUEhh4X",
+          price: ethPrice.market_data.current_price.usd
+        })
+      }
+      
     }
 
 
