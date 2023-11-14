c0b32353f7f586b20db743bdf946d5c8bb753788
Tommy Faldt Pedersen <tommy@faldt.net>
2023-11-14 23:19:23 +0100

    bridge volume workout

--- index.js
+++ index.js
@@ -109,11 +109,192 @@ app.get('/', async (req, res) => {
               price: item.current_price
             })
           }
-
         })
       }
     }
   }
+
+  /* VRSC-ETH Bridge volume  */
+
+  let startBlock = 2793743;
+  let endblock = 2793743 - 1440;
+
+  let vrscReserveIn = 0;
+  let vrscReserveInLastValue = -1;
+  let vrscReserveOut = 0;
+  let vrscReserveOutLastValue = -1;
+  let vrscReserveInDollars = 0;
+
+  let daiReserveIn = 0;
+  let daiReserveInLastValue = -1;
+  let daiReserveOut = 0;
+  let daiReserveOutLastValue = -1;
+  let daiReserveInDollars = 0;
+
+  let mkrReserveIn = 0;
+  let mkrReserveInLastValue = -1;
+  let mkrReserveOut = 0;
+  let mkrReserveOutLastValue = -1;
+  let mkrReserveInDollars = 0;
+
+  let ethReserveIn = 0;
+  let ethReserveInLastValue = -1;
+  let ethReserveOut = 0;
+  let ethReserveOutLastValue = -1;
+  let ethReserveInDollars = 0;
+
+  for (let i = endblock; i < startBlock; i++) {
+    const getcurrencystateResponse = await fetch("http://localhost:9009/multichain/getcurrencystate/bridge.veth/" + i);
+    const getcurrencystateResult = await getcurrencystateResponse.json();
+    const getcurrencystate = getcurrencystateResult.result[0];
+
+    const getcurrencystateVRSC = getcurrencystate.currencystate.currencies.i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV;
+    const getcurrencystateDAI = getcurrencystate.currencystate.currencies.iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM;
+    const getcurrencystateMKR = getcurrencystate.currencystate.currencies.iCkKJuJScy4Z6NSDK7Mt42ZAB2NEnAE1o4;
+    const getcurrencystateETH = getcurrencystate.currencystate.currencies.i9nwxtKuVYX4MSbeULLiK2ttVi6rUEhh4X;
+
+    // VRSC in
+    if (getcurrencystateVRSC.reservein !== vrscReserveInLastValue) {
+      vrscReserveIn = vrscReserveIn + getcurrencystateVRSC.reservein;
+
+      let vrscReserves = 0;
+      let daiReserves = 0;
+
+      getcurrencystate.currencystate.reservecurrencies.forEach((currency) => {
+        if (currency.currencyid === "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV") {
+          vrscReserves = currency.reserves;
+        }
+        if (currency.currencyid === "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM") {
+          daiReserves = currency.reserves;
+        }
+      })
+      vrscReserveInDollars = vrscReserveInDollars + getcurrencystateVRSC.reservein * (daiReserves / vrscReserves);
+    }
+    // VRSC out
+    if (getcurrencystateVRSC.reserveout !== vrscReserveOutLastValue) {
+      vrscReserveOut = vrscReserveOut + getcurrencystateVRSC.reserveout;
+      let vrscReserves = 0;
+      let daiReserves = 0;
+      getcurrencystate.currencystate.reservecurrencies.forEach((currency) => {
+        if (currency.currencyid === "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV") {
+          vrscReserves = currency.reserves;
+        }
+        if (currency.currencyid === "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM") {
+          daiReserves = currency.reserves;
+        }
+      })
+      vrscReserveInDollars = vrscReserveInDollars + getcurrencystateVRSC.reserveout * (daiReserves / vrscReserves);
+    }
+
+    vrscReserveInLastValue = getcurrencystateVRSC.reservein;
+    vrscReserveOutLastValue = getcurrencystateVRSC.reserveout;
+
+    // DAI in
+    if (getcurrencystateDAI.reservein !== daiReserveInLastValue) {
+      daiReserveIn = daiReserveIn + getcurrencystateDAI.reservein;
+
+      let daiReserves = 0;
+      getcurrencystate.currencystate.reservecurrencies.forEach((currency) => {
+        if (currency.currencyid === "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM") {
+          daiReserves = currency.reserves;
+        }
+      })
+      daiReserveInDollars = daiReserveInDollars + getcurrencystateDAI.reservein * 1;
+    }
+    // DAI out
+    if (getcurrencystateDAI.reserveout !== daiReserveOutLastValue) {
+      daiReserveOut = daiReserveOut + getcurrencystateDAI.reserveout;
+
+      let daiReserves = 0;
+      getcurrencystate.currencystate.reservecurrencies.forEach((currency) => {
+        if (currency.currencyid === "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM") {
+          daiReserves = currency.reserves;
+        }
+      })
+      daiReserveInDollars = daiReserveInDollars + getcurrencystateDAI.reserveout * 1;
+    }
+    daiReserveInLastValue = getcurrencystateDAI.reservein;
+    daiReserveOutLastValue = getcurrencystateDAI.reserveout;
+
+    // MKR in
+    if (getcurrencystateMKR.reservein !== mkrReserveInLastValue) {
+      mkrReserveIn = mkrReserveIn + getcurrencystateMKR.reservein;
+
+      let mkrReserves = 0;
+      let daiReserves = 0;
+
+      getcurrencystate.currencystate.reservecurrencies.forEach((currency) => {
+        if (currency.currencyid === "iCkKJuJScy4Z6NSDK7Mt42ZAB2NEnAE1o4") {
+          mkrReserves = currency.reserves;
+        }
+        if (currency.currencyid === "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM") {
+          daiReserves = currency.reserves;
+        }
+      })
+      mkrReserveInDollars = mkrReserveInDollars + getcurrencystateMKR.reservein * (daiReserves / mkrReserves);
+    }
+    // MKR out
+    if (getcurrencystateMKR.reserveout !== mkrReserveOutLastValue) {
+      mkrReserveOut = mkrReserveOut + getcurrencystateMKR.reserveout;
+      let mkrReserves = 0;
+      let daiReserves = 0;
+      getcurrencystate.currencystate.reservecurrencies.forEach((currency) => {
+        if (currency.currencyid === "iCkKJuJScy4Z6NSDK7Mt42ZAB2NEnAE1o4") {
+          mkrReserves = currency.reserves;
+        }
+        if (currency.currencyid === "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM") {
+          daiReserves = currency.reserves;
+        }
+      })
+      mkrReserveInDollars = mkrReserveInDollars + getcurrencystateMKR.reserveout * (daiReserves / mkrReserves);
+    }
+
+    mkrReserveInLastValue = getcurrencystateMKR.reservein;
+    mkrReserveOutLastValue = getcurrencystateMKR.reserveout;
+
+    // ETH in
+    if (getcurrencystateETH.reservein !== ethReserveInLastValue) {
+      ethReserveIn = ethReserveIn + getcurrencystateETH.reservein;
+
+      let ethReserves = 0;
+      let daiReserves = 0;
+
+      getcurrencystate.currencystate.reservecurrencies.forEach((currency) => {
+        if (currency.currencyid === "i9nwxtKuVYX4MSbeULLiK2ttVi6rUEhh4X") {
+          ethReserves = currency.reserves;
+        }
+        if (currency.currencyid === "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM") {
+          daiReserves = currency.reserves;
+        }
+      })
+      ethReserveInDollars = ethReserveInDollars + getcurrencystateETH.reservein * (daiReserves / ethReserves);
+    }
+    // ETH out
+    if (getcurrencystateETH.reserveout !== ethReserveOutLastValue) {
+      ethReserveOut = ethReserveOut + getcurrencystateETH.reserveout;
+      let ethReserves = 0;
+      let daiReserves = 0;
+      getcurrencystate.currencystate.reservecurrencies.forEach((currency) => {
+        if (currency.currencyid === "iCkKJuJScy4Z6NSDK7Mt42ZAB2NEnAE1o4") {
+          ethReserves = currency.reserves;
+        }
+        if (currency.currencyid === "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM") {
+          daiReserves = currency.reserves;
+        }
+      })
+      ethReserveInDollars = ethReserveInDollars + getcurrencystateETH.reserveout * (daiReserves / ethReserves);
+    }
+    ethReserveInLastValue = getcurrencystateETH.reservein;
+    ethReserveOutLastValue = getcurrencystateETH.reserveout;
+  }
+
+  console.log("vrsc total", vrscReserveIn + vrscReserveOut, "$", vrscReserveInDollars);
+  console.log("mkr total", mkrReserveIn + mkrReserveOut, "$", mkrReserveInDollars);
+  console.log("dai total", daiReserveIn + daiReserveOut, "$", daiReserveInDollars);
+  console.log("eth total", ethReserveIn + ethReserveOut, "$", ethReserveInDollars);
+  let totalReserveVolumeInDollars = vrscReserveInDollars + mkrReserveInDollars + daiReserveInDollars + ethReserveInDollars;
+  console.log("total volume $", totalReserveVolumeInDollars);
+
 
   /* VRSC-ETH Bridge reserves */
   const getcurrencyResponse = await fetch("http://localhost:9009/multichain/getcurrency/bridge.veth");
