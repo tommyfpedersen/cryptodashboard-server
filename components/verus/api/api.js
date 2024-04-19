55a147e09b6703bfbc8f225564e978635bbc6063
tommyfaldtpedersen <tope@dr.dk>
2024-04-19 09:53:44 +0200

    Update Verus and vARRR API endpoints

--- components/verus/api/api.js
+++ components/verus/api/api.js
@@ -1,30 +1,54 @@
 async function getMiningInfo() {
-    const getmininginfoResponse = await fetch("http://localhost:9009/mining/getmininginfo")
-    const getmininginfoResult = await getmininginfoResponse.json();
-    const getmininginfo = getmininginfoResult.result;
-    return getmininginfo;
+    try {
+        const getmininginfoResponse = await fetch("http://localhost:9009/mining/getmininginfo")
+        const getmininginfoResult = await getmininginfoResponse.json();
+        const getmininginfo = getmininginfoResult.result;
+        return getmininginfo;
+    } catch (error) {
+        // Handle the error here
+        // console.log("Error fetching mining info:", error);
+        return null;
+    }
 }
 
 async function getBlockSubsidy(block) {
-    const getblocksubsidyResponse = await fetch("http://localhost:9009/mining/getblocksubsidy/" + block);
-    const getblocksubsidyResult = await getblocksubsidyResponse.json();
-    const getblocksubsidy = getblocksubsidyResult.result;
-    return getblocksubsidy;
+    try {
+        const getblocksubsidyResponse = await fetch("http://localhost:9009/mining/getblocksubsidy/" + block);
+        const getblocksubsidyResult = await getblocksubsidyResponse.json();
+        const getblocksubsidy = getblocksubsidyResult.result;
+        return getblocksubsidy;
+    } catch (error) {
+        // Handle the error here
+        // console.log("Error fetching mining info:", error);
+        return null;
+    }
 }
 
 async function getBlock(block) {
-    const getblockResponse = await fetch("http://localhost:9009/blockchain/getblock/" + block);
-    const getblockResult = await getblockResponse.json();
-    const getblock = getblockResult.result;
-    return getblock;
+    try {
+        const getblockResponse = await fetch("http://localhost:9009/blockchain/getblock/" + block);
+        const getblockResult = await getblockResponse.json();
+        const getblock = getblockResult.result;
+        return getblock;
+    } catch (error) {
+        // Handle the error here
+        // console.log("Error fetching mining info:", error);
+        return null;
+    }
 }
 
 async function getPeerInfo() {
-    const getpeerinfoResponse = await fetch("http://localhost:9009/network/getpeerinfo/");
-    const getpeerinfoResult = await getpeerinfoResponse.json();
-    const getpeerinfo = getpeerinfoResult.result;
-    return getpeerinfo;
+    try {
+        const getpeerinfoResponse = await fetch("http://localhost:9009/network/getpeerinfo/");
+        const getpeerinfoResult = await getpeerinfoResponse.json();
+        const getpeerinfo = getpeerinfoResult.result;
+        return getpeerinfo;
+    } catch (error) {
+        // Handle the error here
+        // console.log("Error fetching mining info:", error);
+        return null;
+    }
 }
 
 
-module.exports = { getMiningInfo, getBlockSubsidy, getBlock, getPeerInfo}
+module.exports = { getMiningInfo, getBlockSubsidy, getBlock, getPeerInfo }
