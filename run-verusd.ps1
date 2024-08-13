db968af9d9a4e4756376d7aeffd23c86eb52d5d3
tommyfpedersen <tommy@faldt.net>
2024-08-13 12:08:19 +0200

    power shell scripts

---
+++ run-verusd.ps1
@@ -0,0 +1,5 @@
+Write-Host "Verusd loading..."
+invoke-expression 'cmd /c start powershell -NoExit -Command  { write-host "Verusd"; set-location "C:\Users\Tommy\verus-cli\"; ".\verusd -bootstrap;"}'
+invoke-expression 'cmd /c start powershell -NoExit -Command  { write-host "Verusd vARRR"; set-location "C:\Users\Tommy\verus-cli\"; .\verusd -chain=varrr -reindex;}'
+invoke-expression 'cmd /c start powershell -NoExit -Command  { write-host "Verusd vDEX"; set-location "C:\Users\Tommy\verus-cli\"; .\verusd -chain=vdex -reindex;}'
+#invoke-expression 'cmd /c start powershell -Command { write-host "Hi, new window!"; set-location "C:\"; get-childitem ; sleep 3}'
