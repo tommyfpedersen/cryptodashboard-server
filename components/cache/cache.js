6fd91f346da15a4d95e95bc347ca98a48ad7dfc9
tommyfaldtpedersen <tope@dr.dk>
2024-09-12 14:31:13 +0200

    update time and alert and vdex 30 day fix

--- components/cache/cache.js
+++ components/cache/cache.js
@@ -19,7 +19,22 @@ export async function writeToCache(fileName, dataObject) {
 
 export async function readFromCache(fileName) {
   let result = await client.get("data");
-  return await JSON.parse(result);
+  let cachetimestamp = await client.get("timestamp");
+  let resultParsed = await JSON.parse(result);
+
+  resultParsed.outofsync = Date.now() > (Number(cachetimestamp) + (5*60000)) ? true : false;
+  resultParsed.cachetimestamp = new Date(Number(cachetimestamp)).toLocaleString();
+
+  // Calculate the time difference
+  let now = Date.now();
+  let cacheTime = Number(cachetimestamp);
+  let timeDifference = now - cacheTime;
+  // Convert the time difference to a human-readable format
+  let timeAgo = formatTimeDifference(timeDifference);
+
+  resultParsed.timeAgo = timeAgo;
+
+  return resultParsed;
 
   // try {
   //   const dataString = await fs.readFile(fileName, 'utf8');
@@ -33,12 +48,30 @@ export async function readFromCache(fileName) {
 }
 
 export async function isCacheReady() {
-  let result = false;
+  let result = false
+
   let ready = await client.get("cacheready");
   let cacheTimestamp = await client.get("timestamp");
 
-  if(ready === "true" && Date.now() < cacheTimestamp + 80000){
+  if (ready === "true" && Date.now() < cacheTimestamp + 80000) {
     result = true;
   }
   return result;
+}
+
+function formatTimeDifference(ms) {
+  const seconds = Math.floor(ms / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (days > 0) {
+    return `${days} day${days > 1 ? 's' : ''} ago`;
+  } else if (hours > 0) {
+    return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+  } else if (minutes > 0) {
+    return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+  } else {
+    return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
+  }
 }
