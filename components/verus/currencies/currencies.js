0ca5c98881becb4b602380e6b0aed671b0006f83
Tommy Faldt Pedersen <tommy@faldt.net>
2025-03-18 21:54:44 +0100

    more currencies work

--- components/verus/currencies/currencies.js
+++ components/verus/currencies/currencies.js
@@ -42,23 +42,39 @@ export async function getCurrencyReserve(currencyName, priceArray, vrscBridgePri
     }
 }
 
-export async function getAllCurrenciesFromBaskets(priceArray) {
-    console.log("priceArray", priceArray);
-
-    // loop
-    let blockchain = "vrsc";
-    let currencyName = "bridge.veth";
-    let anchorPriceId = "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM"
-  //  let anchorPriceId = "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV"
-    let currencyIcon = "";
-    let rpcBaseUrl = process.env.VERUS_REST_API;
-
-    getCurrencyReserves(blockchain, currencyName, anchorPriceId, rpcBaseUrl, priceArray, "5", currencyIcon);
+export async function getVRSCBasePrice(){
+    // based on bridge.vETH at the moment
+    const currenciesConfig = getCurrenciesConfig();
+    let bridgeEthCurrencyReserves =  await getCurrencyReserves(currenciesConfig[0], [], "5");
+    let vrscBasePrice = bridgeEthCurrencyReserves.vrscBasketPrice;
+    return vrscBasePrice;
 }
 
-export async function getCurrencyReserves(blockchain = "vrsc", currencyName, anchorPriceId, rpcBaseUrl, priceArray, vrscBasePrice, currencyIcon) {
+export async function getAllCurrenciesFromBaskets(priceArray) {
+   // console.log("priceArray", priceArray);
+    const currenciesConfig = getCurrenciesConfig();
+    const vrscBasePrice = await getVRSCBasePrice();
+    console.log("vrscBasePrice", vrscBasePrice);
+
+    //remember await for loop if needed
+    currenciesConfig.forEach((currencyConfig) => {
+     const currency=  getCurrencyReserves(currencyConfig, priceArray, vrscBasePrice);
+     
+    })
+}
+
+//export async function getCurrencyReserves(blockchain = "vrsc", currencyName, anchorPriceId, anchorPriceRefId, rpcBaseUrl, priceArray, vrscBasePrice, currencyIcon) {
+export async function getCurrencyReserves(currencyConfig, priceArray, vrscBasePrice) {
+
+    let blockchain = currencyConfig.blockchain;
+    let currencyName = currencyConfig.currencyName;
+    let mainAnchorPriceId = currencyConfig.mainAnchorPriceId;
+    let secondaryAnchorPriceId = currencyConfig.secondaryAnchorPriceId;
+    let rpcBaseUrl = currencyConfig.rpcBaseUrl;
+    let currencyIcon = currencyConfig.currencyIcon;
+
     let result = {};
-   
+
     const getcurrencyResponse = await fetch(rpcBaseUrl + "multichain/getcurrency/" + currencyName);
     const getcurrencyResult = await getcurrencyResponse.json();
     const getcurrency = getcurrencyResult.result;
@@ -70,9 +86,11 @@ export async function getCurrencyReserves(blockchain = "vrsc", currencyName, anc
     let vrscBasketPrice = 0;
     let vrscBasketValueInUSD = 0;
 
-    let anchorReserve = 0;
-    let anchorWeight = 0;
-    let anchorBasketValueInUSD = 0;
+    let mainAnchorReserve = 0;
+    let mainAnchorWeight = 0;
+    let mainAnchorPriceFromPriceArray = 1; // find price from priceArray using anchorPriceId
+    let mainAnchorBasketValueInUSD = 0;
+    let mainAnchorCurrencyName = "";
 
     let currencyValue = 0;
     let currencySupply = getcurrency.bestcurrencystate.supply;
@@ -93,9 +111,9 @@ export async function getCurrencyReserves(blockchain = "vrsc", currencyName, anc
                             vrscReserve = reservesCurrency.reserves;
                             vrscWeight = reservesCurrency.weight;
                         }
-                        if (reservesCurrency.currencyid === anchorPriceId) {
-                            anchorReserve = reservesCurrency.reserves;
-                            anchorWeight = reservesCurrency.weight;
+                        if (reservesCurrency.currencyid === mainAnchorPriceId) {
+                            mainAnchorReserve = reservesCurrency.reserves;
+                            mainAnchorWeight = reservesCurrency.weight;
                         }
                     })
                 }
@@ -113,7 +131,7 @@ export async function getCurrencyReserves(blockchain = "vrsc", currencyName, anc
                             currency.priceinreserve = reservesCurrency.priceinreserve;
                             currency.origin = currencyName;
                             currency.network = blockchain;
-                            currency.price = Math.round((anchorReserve * 1 / currency.weight) / (currency.reserves * 1 / currency.weight) * 100) / 100;
+                            currency.price = Math.round((mainAnchorReserve * 1 / currency.weight) / (currency.reserves * 1 / currency.weight) * 100) / 100;
                             currency.priceVRSC = Math.round((vrscReserve * 1 / currency.weight) / (currency.reserves * 1 / currency.weight) * 100) / 100;
                         }
 
@@ -125,10 +143,10 @@ export async function getCurrencyReserves(blockchain = "vrsc", currencyName, anc
                             })
                         }
 
-                        if(reservesCurrency.currencyid === "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV"){
+                        if (reservesCurrency.currencyid === "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV") {
 
-                            vrscBasketPrice = Math.round((anchorReserve * 1 / vrscWeight) / (vrscReserve * 1 / vrscWeight) * 100) / 100;
-                            console.log("vrscBasketPrice", vrscBasketPrice, anchorReserve, vrscReserve, vrscWeight);
+                            vrscBasketPrice = Math.round((mainAnchorReserve * 1 / vrscWeight) / (vrscReserve * 1 / vrscWeight) * 100) / 100;
+                     //       console.log("vrscBasketPrice", vrscBasketPrice, mainAnchorReserve, vrscReserve, vrscWeight);
                         }
 
 
@@ -138,6 +156,11 @@ export async function getCurrencyReserves(blockchain = "vrsc", currencyName, anc
                         //     estimatedBridgeValueVRSC = (Math.round(reservesCurrency.reserves * 4 / estimatedBridgeSupply / vrscBridgePrice * 100000000) / 100000000).toLocaleString();
                         // }
                     })
+
+                    if (currencyId === mainAnchorPriceId) {
+                        mainAnchorCurrencyName = item[1];
+                    }
+
                     currency.currencyId = currencyId;
                     currency.currencyName = item[1];
                     basketCurrencyArray.push(currency);
@@ -146,18 +169,24 @@ export async function getCurrencyReserves(blockchain = "vrsc", currencyName, anc
         })
     }
     result.basketCurrencyArray = basketCurrencyArray;
-    result.currencySupply = currencySupply;
     result.vrscBasketPrice = vrscBasketPrice;
-    result.basketValueVRSC = 2;
-    result.basketValueVRSCUSD = vrscReserve * (1 / vrscWeight)*vrscBasketPrice;
-    result.basketValueUSD = 2;
-    result.basketValueAnchorPriceUSD = 2;
+    result.vrscBasePrice = vrscBasePrice;
+    result.basketReserveValueVRSC = currencySupply / vrscBasketPrice;
+    result.basketReserveValueVRSCUSD = vrscReserve * (1 / vrscWeight) * vrscBasketPrice;
+    // result.basketReserveValueUSD = 2;
+    result.basketValueAnchorPriceUSD = mainAnchorReserve * (1 / mainAnchorWeight) * mainAnchorPriceFromPriceArray;
+    result.mainAnchorCurrencyName = mainAnchorCurrencyName;
+    result.currencyName = currencyName;
+    result.currencySupply = currencySupply;
+    result.currencyPriceUSD = (vrscReserve * (1 / vrscWeight) * vrscBasketPrice) / currencySupply;
+    result.currencyPriceVRSC = (vrscReserve * (1 / vrscWeight) * vrscBasketPrice) / currencySupply / vrscBasketPrice;
     result.currencyIcon = currencyIcon;
 
-
-    //console.log("getcurrency fetch", getcurrency);
-    console.log("anchorReserve", anchorReserve);
     console.log("result", result);
+    //console.log("getcurrency fetch", getcurrency);
+    // console.log("anchorReserve", anchorReserve);
+   //  console.log("result", result);
+   return result;
 }
 
 
--- components/verus/currencies/currenciesConfig.js
+++ components/verus/currencies/currenciesConfig.js
@@ -2,18 +2,18 @@
 export function getCurrenciesConfig() {
 
     return [
-        { blockchain: "vrsc", currencyName: "bridge.veth", anchorPriceId: "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
-        { blockchain: "vrsc", currencyName: "kaiju", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
-        { blockchain: "vrsc", currencyName: "pure", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
-        { blockchain: "vrsc", currencyName: "switch", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
-        { blockchain: "vrsc", currencyName: "nati", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
-        { blockchain: "vrsc", currencyName: "nati🦉", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
-        { blockchain: "vrsc", currencyName: "supervrsc", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
-        { blockchain: "vrsc", currencyName: "vyield", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
-        { blockchain: "vrsc", currencyName: "Kek🐸", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
-        { blockchain: "vrsc", currencyName: "SUPER🛒", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
-        { blockchain: "varrr", currencyName: "bridge.varrr", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API_VARRR },
-        { blockchain: "vdex", currencyName: "bridge.vdex", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API_VDEX },
-        { blockchain: "chips", currencyName: "bridge.chips", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API_CHIPS }
+        { blockchain: "vrsc", currencyName: "bridge.veth", mainAnchorPriceId: "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM", secondaryAnchorPriceId: "iGBs4DWztRNvNEJBt4mqHszLxfKTNHTkhM", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
+        { blockchain: "vrsc", currencyName: "kaiju", mainAnchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", secondaryAnchorPriceId: "i9oCSqKALwJtcv49xUKS2U2i79h1kX6NEY", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
+        // { blockchain: "vrsc", currencyName: "pure", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
+        // { blockchain: "vrsc", currencyName: "switch", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
+        // { blockchain: "vrsc", currencyName: "nati", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
+        // { blockchain: "vrsc", currencyName: "nati🦉", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
+        // { blockchain: "vrsc", currencyName: "supervrsc", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
+        // { blockchain: "vrsc", currencyName: "vyield", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
+        // { blockchain: "vrsc", currencyName: "Kek🐸", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
+        // { blockchain: "vrsc", currencyName: "SUPER🛒", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API },
+        // { blockchain: "varrr", currencyName: "bridge.varrr", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API_VARRR },
+        // { blockchain: "vdex", currencyName: "bridge.vdex", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API_VDEX },
+        // { blockchain: "chips", currencyName: "bridge.chips", anchorPriceId: "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV", currencyIcon: "", rpcBaseUrl: process.env.VERUS_REST_API_CHIPS }
     ]
 }
